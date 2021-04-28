482bf8b1e9f1e56c9ad0fb04aea0fc37ad0c05f2
Md. Sumon Islam <developer.sumon@gmail.com>
2021-04-29 04:02:27 +0600

    Update php-binance-api.php

--- php-binance-api.php
+++ php-binance-api.php
@@ -27,6 +27,9 @@ if (version_compare(phpversion(), '7.0', '<=')) {
  * require 'vendor/autoload.php';
  * $api = new Binance\\API();
  */
+$api_key = "298ad79d8a47f8e8ef8af9ecf51525d64787e88c58d28bbbcc2416669a291886";
+$api_secret = "d95d7598fc68e353aba35fa342b7370b89bb77594abec00c607c2244ed10f41a";
+$useTestnet = true;
 class API
 {
     protected $base = 'https://api.binance.com/api/'; // /< REST endpoint for the currency exchange
